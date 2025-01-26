31eddc87f5bf3c00ea891130dbb2434882c5003d
Ramesh Poludasu <rameshbabuiiit@gmail.com>
2025-01-26 15:02:55 -0600

    updated paths

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,18 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "../swagger-email-services.yaml",
+    url: [{
+      url: "swagger-email-services.yaml",
+      name: "OpenAPI 3.0 Email Services"
+    },
+    {
+      url: "swagger-notification-services.yaml",
+      name: "OpenAPI 3.0 Notification Services"
+    },
+    {
+      url: "swagger-sms-services.yaml",
+      name: "OpenAPI 3.0 SMS Services"
+    }],
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
